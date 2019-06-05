743367ac2421345746a037f799ccb4076030e63a
Baljeet Singh <baljeetsinghipu@gmail.com>
2019-06-05 09:32:10 -0400

    InValidParentheses

---
+++ genericCode/InvalidParentheses.java
@@ -0,0 +1,62 @@
+package genericCode;
+
+import java.util.*;
+
+public class InvalidParentheses {
+	
+	List<String> parenthesesCorrector(String str)
+	{
+		ArrayList<String> result = new ArrayList<String>();
+		
+		helper(str,result,0,0,'(',')');
+		
+		return result;
+	}
+	
+	void helper(String str, List<String> result,int i,int j,char open,char close)
+	{
+		int closeNum=0;
+		int openNum=0;
+		for(int k=i;k<str.length();k++)
+		{
+			
+			if(str.charAt(k)==close) closeNum++;
+			else if(str.charAt(k)==open) openNum++;
+			if(closeNum>openNum) 
+			{
+				for(int m=j;m<k;m++) 
+				{
+					if(str.charAt(m)==close && (str.charAt(m-1)!=close || m==j)) 
+					{
+						helper(str.substring(0,m)+str.substring(m+1,str.length()),result,i,j,open,close);
+					}
+				}
+				return;
+			}
+		}
+			
+			String str1= new StringBuilder(str).reverse().toString();
+			
+			if(open=='(') helper(str1,result,0,0,')','(');
+			else result.add(str1);
+			
+		}
+		
+
+
+	public static void main(String[] args) {
+		InvalidParentheses obj = new InvalidParentheses();
+		String str = "()())()";
+		List<String> result=obj.parenthesesCorrector(str);
+		
+		for(String s : result) 
+		{
+			System.out.print(s);
+			System.out.println(" ");
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
